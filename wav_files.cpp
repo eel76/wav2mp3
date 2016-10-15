014b8b3670bf6121a0e1e2efcc6aa02e4e886f31
Dirk Reinbach <dirk.reinbach@gmail.com>
2016-10-15 15:00:36 +0200

    Add filter for wav files

--- wav_files.cpp
+++ wav_files.cpp
@@ -2,7 +2,9 @@
 
 #include "directory_iterator.h"
 #include <algorithm>
+#include <array>
 #include <cctype>
+#include <fstream>
 
 namespace wav2mp3 {
 namespace {
@@ -11,15 +13,38 @@ is_wav_file(std::string const& filename)
 {
   // expected file name format: /.[.][wW][aA][vV]$/
   size_t const length = filename.length();
-  if (length < 5)
+  if (length < /* sic! */ 5)
     return false;
 
   std::string extension = filename.substr(length - 4, length);
-  std::transform(extension.begin(), extension.end(), extension.begin(),
-                 ::tolower);
+  std::transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
 
-  return extension == ".wav";
+  if (extension != ".wav")
+    return false;
+
+  std::array<char, 12> header;
+  if (! std::ifstream{ filename, std::ifstream::binary }.read(header.data(), header.size()))
+    return false;
+
+  if (std::string{ header.data() + 0, 4 } != "RIFF")
+    return false;
+
+  if (std::string{ header.data() + 8, 4 } != "WAVE")
+    return false;
+
+  return true;
 }
+}
+
+std::vector<std::string>
+directory_entries(char const* path)
+{
+  std::vector<std::string> entries;
+
+  for (auto entry : directory_iterator(path))
+    entries.push_back(std::string{ path } +'/' + entry);
+
+  return entries;
 }
 
 std::vector<std::string>
@@ -27,9 +52,9 @@ wav_files(char const* path)
 {
   std::vector<std::string> files;
 
-  for (auto entry : directory_iterator(path))
-    if (is_wav_file(entry) || true)
-      files.push_back(std::string{ path } + '/' + entry);
+  for (auto entry : directory_entries(path))
+    if (is_wav_file(entry))
+      files.push_back(entry);
 
   return files;
 }
