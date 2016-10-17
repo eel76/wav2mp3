295d5ef08cfcd0e686cdeaba9c0d8fdf96e7fbf4
Dirk Reinbach <reinbach@volumegraphics.com>
2016-10-17 17:45:21 +0200

    Rename lame project file

--- lame.cmake
+++ lame.cmake
@@ -1,12 +1,10 @@
 ExternalProject_Add(lame
   URL http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/lame-3.99.5
-  PATCH_COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${CMAKE_CURRENT_SOURCE_DIR}/lame-CMakeLists.txt" "${CMAKE_CURRENT_BINARY_DIR}/lame-3.99.5/src/lame/CMakeLists.txt"
+  PATCH_COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists-lame.txt" "${CMAKE_CURRENT_BINARY_DIR}/lame-3.99.5/src/lame/CMakeLists.txt"
   COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${CMAKE_CURRENT_BINARY_DIR}/lame-3.99.5/src/lame/configMS.h" "${CMAKE_CURRENT_BINARY_DIR}/lame-3.99.5/src/lame/config.h"
   INSTALL_COMMAND ""
 )
-
-set_target_properties(lame PROPERTIES FOLDER "ExternalProjects")
 
 ExternalProject_Get_Property(lame source_dir)
 ExternalProject_Get_Property(lame binary_dir)
